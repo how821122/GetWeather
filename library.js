45036b05b540277676d40cdc36d9ec9f30d5119e
Kuo <stone821122@gmail.com>
2017-07-11 20:44:41 +0800

    Version 1.1
    Prove the library

--- library.js
+++ library.js
@@ -1,29 +1,83 @@
-var request = require('request');
-var area = require("./label_area.js");
-//----------------------------------------------------------//
-//---------------整理每三個小時的數據-----------------------//
-//----------------------------------------------------------//
-//DataArray=抓下來尚未整理的資料
-//DayOfWeek= 因為星期X給會隨時間變化推移,需要重新進行判斷
-//After_2XHour==> "現在"標籤 =2,  經過 (X-2)*3小時
-function per3Hdata(DataArray,DayOfWeek,AfterXHour){
-  
-   var Weather =new Object(); 
+function preprocess_Data(DataArray,Meteorology){
+//---Date array
+//0,1,2  three day
+  var date = DataArray[0];
+  var date_array=[];
+  for(var i =2;i<5;i++){
+    date_array[i-2]=date[i].trim();
+  }
+ //console.log(date_array);
 
-       Weather.DayOfWeek=DataArray[0][DayOfWeek];//日期要排序
-       Weather.time=DataArray[1][AfterXHour];
-       Weather.Tempure=DataArray[3][AfterXHour];
-       Weather.Tempure_feel=DataArray[4][AfterXHour];
-       Weather.wind_direction=DataArray[6][AfterXHour];
-       Weather.relative_humidity=DataArray[7][AfterXHour];
-       //Weather.rain=DataArray[8][AfterXHour],//每六個小時才算一次
-       Weather.feeling=DataArray[9][AfterXHour];
+//---time array
+  var time = DataArray[1];
+  var time_array=[];
+//---tempure array
+  var tempure = DataArray[3];
+  var tempure_array=[];
+//---tempure felt array
+  var tempure_felt = DataArray[4];
+  var tempure_felt_array = [];
+//---wind_direction array
+  var wind_direction = DataArray[6];
+  var wind_direction_array=[];
+//---relative_humidity array
+  var relative_humidity = DataArray[7];
+  var relative_humidity_array =[];
+//---rain array
+  var rain_percent = DataArray[8];
+  var rain_percent_array=[];
+//---feeling array
+  var feeling = DataArray[9];
+  var feeling_array=[];
 
-       return Weather;
+//---------------------------------------------
+  for(var j =2;j<19;j++){
+    time_array[j-2]=time[j].trim();
+    tempure_array[j-2]=tempure[j].trim();
+    tempure_felt_array[j-2]=tempure_felt[j].trim();
+    wind_direction_array[j-2] = wind_direction[j].trim();
+    relative_humidity_array[j-2] =relative_humidity[j].trim();
+    feeling_array[j-2] = feeling[j].trim();
+  }
+ 
+var weather = Meteorology;
+
+   //console.log(Meteorology);
+   //
+  var alldata = [];
+  var w=0;//count for date (0,1,2) 
+
+
+  for(var k=0;k<17;k++){
+
+    if(time_array[k]!="00:00"){
+       
+       alldata[k]=[ date_array[w],time_array[k],tempure_array[k],
+                    tempure_felt_array[k],wind_direction_array[k],relative_humidity_array[k],
+                    feeling_array[k],weather[k]];
+    }
+    else{
+      w=w+1;
+      alldata[k]=[ date_array[w],time_array[k],tempure_array[k],
+                    tempure_felt_array[k],wind_direction_array[k],relative_humidity_array[k],
+                    feeling_array[k],weather[k]];
+    }
+  }
+ 
+  return alldata ;
+  //-----
+  //  alldata[x][0]:date,
+  //  alldata[x][1]:time,
+  //  alldata[x][2]:tempure,
+  //  alldata[x][3]:tempure_felt,
+  //  alldata[x][4]:wind_direction,
+  //  alldata[x][5]:relative_humidity,
+  //  alldata[x][6]:feeling_array,
+  //  alldata[x][7]:weather
+  //-----
+ 
  }
-//-----------------------------------------------------------------------------------------------------
 
-//--------------------------------------------------------------------------------------
 
-exports.per3Hdata = per3Hdata;
-//exports.print_total = print_total;
+exports.preprocess_Data = preprocess_Data;
+ 
--- main.js
+++ main.js
@@ -2,53 +2,37 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs =require('fs');
 var library = require("./library.js");
-var area = require("./label_area.js");
+var Area = require("./label_area.js");
+ 
 
 
 
-var url = "http://www.cwb.gov.tw/V7/forecast/town368/3Hr/6301000.htm";//內湖
+var url =  "http://www.cwb.gov.tw/V7/forecast/town368/3Hr/"+6301100+".htm";
 
 request(url, function(err, res, body){
-    // 去跟中央氣象局的網站要資料
-   
-  var $ = cheerio.load(body);
-    // 把要到的資料放進 cheerio
 
-	var Data_Sourse = []
-    $('.Forecast-box tr').each(function(i, elem){
+    var $ = cheerio.load(body);
+//---Get Date & Time
+    var Data_Sourse = [];
+    $('.Forecast-box tr ').each(function(i, elem){
         Data_Sourse.push($(this).text().split('\n'));
     });
-     
-    var district = area.Area(url);
+//----Get Weather
+    var Meteorology = [];
+    $('img').each(function(i, elem){
+        Meteorology.push($(this).attr('title'));
+    });
+//----Print data 
+    var area = Area.getArea(url);
+    var weather_data = library.preprocess_Data(Data_Sourse,Meteorology);
+    
+    var total_data = weather_data.concat(area);
 
-    var W = per3Hdata(Data_Sourse,2,X)
+    console.log(total_data);
 
-    console.log(district,Weather_Data);
+    //-----17:地區
 
-    fs.writeFileSync("result.json", JSON.stringify(Weather_Data,",",3));
-
+});
  
-})
 
-//----------------------------------------------------------------------------------------
-function print_total(Data_Sourse){
 
-    var DayOfWeek=2;
-    var section =2;
-    var W= new Object();
-
-  for(section =2;section<=18;section++){
-     var X = per3Hdata(Data_Sourse,DayOfWeek,section).time.substring(1,6);//幾點，若是00:00--->日期+1
-   //-------------------------------判斷是否換日  
-     if(X==00){
-      DayOfWeek=DayOfWeek+1;
-      }
-    else{
-      DayOfWeek=DayOfWeek; 
-      }
-
-    //----------------------------
-     W[section-2] = per3Hdata(Data_Sourse,DayOfWeek,section);
-    }
-    return W ;//回傳一個陣列作為輸出
-}
