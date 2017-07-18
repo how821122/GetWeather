4f876565f8c9467a551592037d40e358e5912f95
Kuo <stone821122@gmail.com>
2017-07-18 18:49:59 +0800

    Version 1.2
    add loop

--- label_area.js
+++ label_area.js
@@ -2,47 +2,47 @@
 function getArea(url){
 
 	
-    var X = url.substring(46,53);//get the district code form url
-    var number =Number(X) ;
+    var number = url.substring(46,53);//get the district code form url
+ 
     var district;
 	
 
-	if (number=6300100) {
+	if (number=="6300100") {
 		district= "松山" 
 	}
 		 
-	else if(number=6300200){
+	else if(number=="6300200"){
 		district = "信義" ;
 	}
 
-	else if(number=6300300){
+	else if(number=="6300300"){
 		district = "大安";
 	}
-	else if(number=6300400){
+	else if(number=="6300400"){
 		district = "中山" ;
 	}
-	else if(number=6300500){
+	else if(number=="6300500"){
 		district = "中正" ;
 	}
-	else if(number=6300600){
+	else if(number=="6300600"){
 		district = "大同" ;
 	}
-	else if(number=6300700){
+	else if(number=="6300700"){
 		district = "萬華" ;
 	}
-	else if(number=6300800){
+	else if(number=="6300800"){
 		district = "文山" ;
 	}		 
-	else if(number=6300900){
+	else if(number=="6300900"){
 		district = "南港" ;
 	}	         
-	else if(number=6301000){
+	else if(number=="6301000"){
 		district = "內湖" ;
 	}	
-	else if(number=6301100){
+	else if(number=="6301100"){
 		district = "士林" ;
 	}	
-	else if(number=6301200){
+	else if(number=="6301200"){
 		district = "北投" ;
 	}	
     
@@ -51,7 +51,8 @@ function getArea(url){
     	district = "no result" ;
     }
 
-    return district ;
+    //var X = JSON.parse(district);
+    return  district;
  
 }
 
--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs= require('fs');
-
+var GetDistrict = require("./label_area.js");
 var library = require("./library.js");
 
-var Area = library.getURL();
+var url = library.getURL();
+ 
+ for(var k=0;k<12;k++){ 
+        loop(k,k*1000);
+      }
+ 
+setInterval(function(){
+       
+  for(var k=0;k<12;k++){ 
+        loop(k,k*1000);
+      }
+},10800000);//3hr = 1000ms * 60 * 60 * 3 =10800000ms
+
  
 
-  request(Area[1], function(err, res, body){
+//------------------------------------------------------------------
+ 
+function loop(k,time){
+  setTimeout(function WW(){
+  Get(url[k]);
+ },time);
+}
+
+
+ //-------------------------------------------------------------------
+function Get(url){
+
+   request(url, function(err, res, body){
 
     var $ = cheerio.load(body);
  
@@ -21,29 +45,22 @@ var Area = library.getURL();
         Meteorology.push($(this).attr('title'));
     });
  
-   var total_data =  library.preprocess_Data(Data_Sourse,Meteorology,Area[1]);//Array 
+   var total_data =  library.preprocess_Data(Data_Sourse,Meteorology,url);//Array 
 
    var file = library.Convertjson(total_data);//json file
 
    var jsonText = JSON.stringify(file[0]," "," ");
-   console.log(file);
+   console.log(jsonText);
   
-
-  //fs.appendFile("result.json",total_data);//持續寫入
+ 
+  fs.appendFile("result.json",jsonText);//持續寫入
   //fs.writeFile("result.json","XX");//覆蓋寫入
   //
   //
- });
+  });
+}
 
 
-//--
-/*
-setInterval(function(){
-    
-},10000);
-
-*/
-//--
 
 
 
