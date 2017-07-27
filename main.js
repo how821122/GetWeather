51c947d577bf1caa557b2d58f1529ebe053203fa
how821122 <stone821122@gmail.com>
2017-07-27 15:55:05 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs= require('fs');
-
+var GetDistrict = require("./label_area.js");
 var library = require("./library.js");
 
-var Area = library.getURL();
+var url = library.getURL();
+
+ 
+ //---start for first data
+ for(var k=0;k<12;k++){ 
+        loop(k,k*1000);
+      }
+ //---loop
+setInterval(function(){
+       
+  for(var k=0;k<12;k++){ 
+        loop(k,k*1000);
+      }
+},10800000);//3hr = 1000ms * 60 * 60 * 3 =10800000ms
+
  
 
-  request(Area[1], function(err, res, body){
+//------------------------------------------------------------------
+
+function loop(k,time){
+
+  var url = library.getURL()//array
+  var area=[];
+  
+  area[k] = GetDistrict.getArea(url[k]);//determine the area by url
+  
+  setTimeout(function Delayfunction(){
+ 
+  Get(url[k],area[k]);
+ },time); 
+
+}
+
+
+ //-------------------------------------------------------------------
+function Get(url,file_name){
+
+   request(url, function(err, res, body){
 
     var $ = cheerio.load(body);
  
@@ -21,29 +55,22 @@ var Area = library.getURL();
         Meteorology.push($(this).attr('title'));
     });
  
-   var total_data =  library.preprocess_Data(Data_Sourse,Meteorology,Area[1]);//Array 
+   var total_data =  library.preprocess_Data(Data_Sourse,Meteorology,url);//Array 
 
    var file = library.Convertjson(total_data);//json file
 
-   var jsonText = JSON.stringify(file[0]," "," ");
-   console.log(file);
+   var jsonText = JSON.stringify(file[1]," "," ");//only first data
+   console.log(jsonText);
   
-
-  //fs.appendFile("result.json",total_data);//持續寫入
-  //fs.writeFile("result.json","XX");//覆蓋寫入
+ 
+  fs.appendFile("./result/_result_"+file_name+".json",jsonText); //持續寫入
+  //fs.writeFile("./result/result.json",jsonText);//覆蓋寫入
   //
   //
- });
+  });
+}
 
 
-//--
-/*
-setInterval(function(){
-    
-},10000);
-
-*/
-//--
 
 
 
